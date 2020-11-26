d033193e96691221b27840129a5830d4de8d9774
Brian ONeill <Boneill3@users.noreply.github.com>
2020-11-26 09:30:11 -0800

    working sum speed and volume

--- script.py
+++ script.py
@@ -11,23 +11,24 @@ sc = spark.sparkContext
 sql = SQLContext(sc)
 
 # Create dataframes
-df_freeway_detectors = (sql.read
-                        .format("com.databricks.spark.csv")
-                        .option("header", "true")
-                        .option("inferSchema", True)
-                        .load("freeway_detectors.csv"))
+df_detectors = (sql.read
+                .format("com.databricks.spark.csv")
+                .option("header", "true")
+                .option("inferSchema", True)
+                .load("freeway_detectors.csv"))
 
-df_freeway_loopdata = (sql.read
-                       .format("com.databricks.spark.csv")
-                       .option("header", "true")
-                       .option("inferSchema", True)
-                       .load("freeway_loopdata.csv"))
+df_loopdata = (sql.read
+               .format("com.databricks.spark.csv")
+               .option("header", "true")
+               .option("inferSchema", True)
+               .option("timestampFormat", "yyyy-MM-dd HH:mm:ssx")
+               .load("freeway_loopdata.csv"))
 
-df_freeway_stations = (sql.read
-                       .format("com.databricks.spark.csv")
-                       .option("header", "true")
-                       .option("inferSchema", True)
-                       .load("freeway_stations.csv"))
+df_stations = (sql.read
+               .format("com.databricks.spark.csv")
+               .option("header", "true")
+               .option("inferSchema", True)
+               .load("freeway_stations.csv"))
 
 df_highways = (sql.read
                .format("com.databricks.spark.csv")
@@ -35,41 +36,46 @@ df_highways = (sql.read
                .option("inferSchema", True)
                .load("highways.csv"))
 
-# Drop unwanted columns
-df_detector = df_detector.drop("milepost", "detectortitle", "lanenumber",
-                               "agency_lane", "active_dates", "enabledflag",
-                               "detectortype", "detectorclass",
-                               "detectorstatus", "rampid", "controllerid",
-                               "start_date", "end_date", "atms_id",
-                               "active_dates", "locationtext")
+df_detectors.printSchema()
+df_loopdata.printSchema()
+df_stations.printSchema()
+df_highways.printSchema()
 
-df_station = df_station.drop("milepost", "length", "numberlanes", "agencyid",
-                             "active_dates", "numberlanes", "length_mid",
-                             "downstream_mile", "upstream_mile", "agencyid",
-                             "opposite_stationid", "segment_geom",
-                             "station_geom", "start_date", "end_date",
-                             "detectortype", "agency", "region", "active_dates",
-                             "id", "station_location_id", "detectorlocation",
-                             "upstream", "downstream")
+df_readings = df_loopdata.join(df_detectors, on=['detectorid'], how='full')
 
-# Rename a column so that the joins will match on that column
-df_detector = df_detector.withColumnRenamed('detectorid', 'detector_id')
+df_readings = df_readings.select(df_readings.stationid,
+                                 df_loopdata.volume, df_loopdata.speed).groupBy(
+    df_readings.stationid, window(df_readings.starttime, "5 minutes").alias(
+        "time_window")) \
+    .agg(sum(df_loopdata.volume).alias("total_volume"),
+         sum(when(df_loopdata.speed.isNotNull(),
+                  df_loopdata.speed).otherwise(lit(0))).alias("speed"))
+
+#df_readings = df_readings.select(df_readings.time_window,
+#                                 df_readings.stationid,
+#                                 df_readings.total_volume,
+#                                 df_readings.total_speed).withColumn(
+#    "total_speed", (df_readings.total_volume * df_readings.total_speed))
+
+
+# SELECT WHERE > 80
+# SELECT WHERE < 5
+# Join with readings
+
+df_readings.show(5, False)
 
 # Join the dataframes
-df_first = df_highway.join(df_detector, on=['detector_id'], how='full')
 
 # Drop highwayid to avoid duplicate
-df_first = df_first.drop("highwayid")
+# df_first = df_first.drop("highwayid")
 
 # Join to get the final result
-df = df_first.join(df_station, on=['stationid'], how='full')
+# df = df_first.join(df_station, on=['stationid'], how='full')
 
 # Changes by Brian
-df = df.select(df.starttime, to_timestamp(df.starttime).alias(
-    'timestamp'), df.volume).where(df.starttime.isNotNull())
-df.printSchema()
-df.groupBy(df.starttime).agg(sum(df.volume)).show()
-df.show()
+# df.printSchema()
+# df.groupBy(df.starttime).agg(sum(df.volume)).show()
+# df.show()
 
 # Show the result
 # df.show()
