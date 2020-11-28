c3c18cd87582b3544d7a53885bb34a9284aa3ee3
Brian ONeill <Boneill3@users.noreply.github.com>
2020-11-27 16:19:07 -0800

    Working pyspark

--- script.py
+++ script.py
@@ -15,70 +15,67 @@ df_detectors = (sql.read
                 .format("com.databricks.spark.csv")
                 .option("header", "true")
                 .option("inferSchema", True)
-                .load("freeway_detectors.csv"))
+                .load("data/freeway_detectors.csv"))
 
 df_loopdata = (sql.read
                .format("com.databricks.spark.csv")
                .option("header", "true")
                .option("inferSchema", True)
                .option("timestampFormat", "yyyy-MM-dd HH:mm:ssx")
-               .load("freeway_loopdata.csv"))
+               .load("data/freeway_loopdata.csv"))
 
 df_stations = (sql.read
                .format("com.databricks.spark.csv")
                .option("header", "true")
                .option("inferSchema", True)
-               .load("freeway_stations.csv"))
+               .load("data/freeway_stations.csv"))
 
 df_highways = (sql.read
                .format("com.databricks.spark.csv")
                .option("header", "true")
                .option("inferSchema", True)
-               .load("highways.csv"))
-
-df_detectors.printSchema()
-df_loopdata.printSchema()
-df_stations.printSchema()
-df_highways.printSchema()
+               .load("data/highways.csv"))
 
 df_readings = df_loopdata.join(df_detectors, on=['detectorid'], how='full')
+df_readings = df_readings.fillna(0)
 
 df_readings = df_readings.select(df_readings.stationid,
-                                 df_loopdata.volume, df_loopdata.speed).groupBy(
+                                 df_readings.volume, df_readings.speed,
+                                 ).groupBy(
     df_readings.stationid, window(df_readings.starttime, "5 minutes").alias(
         "time_window")) \
-    .agg(sum(df_loopdata.volume).alias("total_volume"),
-         sum(when(df_loopdata.speed.isNotNull(),
-                  df_loopdata.speed).otherwise(lit(0))).alias("speed"))
+    .agg(sum(when(df_readings.volume.isNotNull(),
+                  df_readings.volume).otherwise(lit(0))).alias("total_volume"),
+         sum(df_readings.speed * df_readings.volume).alias("total_speed"),
+         sum(when(df_readings.speed > 80, df_readings.volume).otherwise(lit(
+             0))).alias(
+             "Speed>80"),
+         sum(when((df_readings.speed < 5) & (df_readings.speed > 0),
+                  df_readings.volume).otherwise(lit(0))).alias(
+             "speed<5"))
 
-#df_readings = df_readings.select(df_readings.time_window,
-#                                 df_readings.stationid,
-#                                 df_readings.total_volume,
-#                                 df_readings.total_speed).withColumn(
-#    "total_speed", (df_readings.total_volume * df_readings.total_speed))
+df_readings = df_readings.select(df_readings.stationid,
+                                 df_readings.time_window.start.alias(
+                                     "startdate"),
+                                 df_readings.time_window.end.alias("enddate"),
+                                 df_readings.total_volume,
+                                 df_readings.total_speed,
+                                 df_readings["speed>80"],
+                                 df_readings["speed<5"])
 
+df_readings.show(20, False)
 
-# SELECT WHERE > 80
-# SELECT WHERE < 5
-# Join with readings
+df_metadata = df_highways.join(df_stations, on=['highwayid'], how='full')
+df_metadata = df_metadata.select(df_metadata.stationid,
+                                 df_metadata.locationtext,
+                                 df_metadata.milepost,
+                                 df_metadata.length,
+                                 df_metadata.direction,
+                                 df_metadata.upstream,
+                                 df_metadata.downstream)
 
-df_readings.show(5, False)
-
-# Join the dataframes
-
-# Drop highwayid to avoid duplicate
-# df_first = df_first.drop("highwayid")
-
-# Join to get the final result
-# df = df_first.join(df_station, on=['stationid'], how='full')
-
-# Changes by Brian
-# df.printSchema()
-# df.groupBy(df.starttime).agg(sum(df.volume)).show()
-# df.show()
-
-# Show the result
-# df.show()
+df_metadata.show()
 
 # Convert to CSV
-# df.coalesce(1).write.option("header", "true").csv("processed_data.csv")
+df_readings.coalesce(1).write.option("header", "true").csv("aggregated.csv")
+df_readings.coalesce(1).write.option("header", "true").csv("metadata.csv")
